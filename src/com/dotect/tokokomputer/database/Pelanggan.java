b6dc39eaf800ec87c04bf0fb474e9c2891fa03cf
YOGA <yogaibnudinataa@gmail.com>
2018-12-21 18:31:04 +0700

    koneksi database

--- src/com/dotect/tokokomputer/database/Pelanggan.java
+++ src/com/dotect/tokokomputer/database/Pelanggan.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.dotect.tokokomputer.entity;
+package com.dotect.tokokomputer.database;
 
 import java.util.Objects;
 
