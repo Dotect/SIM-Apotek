e2b7401648d9acc6a04cd708e2ebda7aac0b9b57
YOGA <yogaibnudinataa@gmail.com>
2018-12-21 18:39:58 +0700

    perbaikan pelanggan

--- src/com/dotect/tokokomputer/entitiy/Pelanggan.java
+++ src/com/dotect/tokokomputer/entitiy/Pelanggan.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.dotect.tokokomputer.database;
+package com.dotect.tokokomputer.entitiy;
 
 import java.util.Objects;
 
