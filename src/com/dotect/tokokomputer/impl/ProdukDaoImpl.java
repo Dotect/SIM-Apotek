8bf947cb10a44a3a764b69dd3986058dd71c324d
KevinRisqi <kevinrisqi22@gmail.com>
2018-12-24 00:04:36 +0700

    Membuat class ProdukDaoImpl dari implementasi interface ProdukDao

---
+++ src/com/dotect/tokokomputer/impl/ProdukDaoImpl.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dotect.tokokomputer.impl;
+
+import com.dotect.tokokomputer.entity.Produk;
+import com.dotect.tokokomputer.error.ProdukException;
+import com.dotect.tokokomputer.service.ProdukDao;
+import java.util.List;
+
+/**
+ *
+ * @author Anonymous
+ */
+public class ProdukDaoImpl implements ProdukDao{
+
+    @Override
+    public void insertProduk(Produk produk) throws ProdukException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void updateProduk(Produk produk) throws ProdukException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void deleteProduk(Integer id) throws ProdukException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void getProduk(Integer id) throws ProdukException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void getProduk(String nama) throws ProdukException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Produk> SelectAllProduk() throws ProdukException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
