381746b4fc47fe0f93a61b8d8a7bd7b2390226d7
KevinRisqi <kevinrisqi22@gmail.com>
2018-12-24 01:11:38 +0700

    Menerapkan query insertProduk

--- src/com/dotect/tokokomputer/impl/ProdukDaoImpl.java
+++ src/com/dotect/tokokomputer/impl/ProdukDaoImpl.java
@@ -9,6 +9,8 @@ import com.dotect.tokokomputer.entity.Produk;
 import com.dotect.tokokomputer.error.ProdukException;
 import com.dotect.tokokomputer.service.ProdukDao;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 /**
@@ -18,8 +20,8 @@ import java.util.List;
 public class ProdukDaoImpl implements ProdukDao{
 
     private Connection connection;
-    private final String insertProduk = "INSERT INTO pelanggan (email_pelanggan,password_pelanggan,nama_pelanggan,telepon)"
-            + "VALUES (?,?,?,?)";
+    private final String insertProduk = "INSERT INTO produk (nama_produk,harga,stok,berat,deskripsi,id_kategori)"
+            + "VALUES (?,?,?,?,?,?)";
             
 
     public ProdukDaoImpl(Connection connection) {
@@ -30,7 +32,22 @@ public class ProdukDaoImpl implements ProdukDao{
     
     @Override
     public void insertProduk(Produk produk) throws ProdukException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        PreparedStatement statement = null;
+        try {
+           statement = connection.prepareStatement(insertProduk);
+           statement.setString(1, produk.getNama_produk());
+           statement.setInt(2, produk.getHarga());
+           statement.setInt(3, produk.getStok());
+           statement.setInt(4, produk.getBerat());
+           statement.setString(5, produk.getDeskripsi());
+           statement.setInt(6, produk.getId_kategori());
+           statement.executeUpdate();
+           
+        } catch (SQLException e) {
+            throw new ProdukException(e.getMessage());
+        } finally {
+            
+        }
     }
 
     @Override
