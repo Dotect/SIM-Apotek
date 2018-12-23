4113c1f27a9d8f42d5f8f9fc34e37a44da71efcf
KevinRisqi <kevinrisqi22@gmail.com>
2018-12-24 00:24:09 +0700

    Membuat objek produk yang nantinya bisa di akses oleh semua class dengan konsep singleton

--- src/com/dotect/tokokomputer/database/TokoKomputerDatabase.java
+++ src/com/dotect/tokokomputer/database/TokoKomputerDatabase.java
@@ -5,6 +5,9 @@
  */
 package com.dotect.tokokomputer.database;
 
+import com.dotect.tokokomputer.entity.Pelanggan;
+import com.dotect.tokokomputer.impl.ProdukDaoImpl;
+import com.dotect.tokokomputer.service.ProdukDao;
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -15,6 +18,7 @@ import java.sql.SQLException;
  */
 public class TokoKomputerDatabase {
     private static Connection connection;
+    private static ProdukDao produkDao;
     
     public static Connection getConnection() throws SQLException{
         if (connection==null) {
@@ -26,5 +30,12 @@ public class TokoKomputerDatabase {
         }
         
         return connection;
+    }
+    
+    public static ProdukDao getProdukDao() throws SQLException{
+        if (produkDao == null) {
+            produkDao = new ProdukDaoImpl(getConnection());
+        }
+        return produkDao;
     }
 }
