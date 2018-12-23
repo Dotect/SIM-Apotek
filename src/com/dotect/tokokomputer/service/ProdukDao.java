e362a80a5151286316ea233280aee9f07c642518
KevinRisqi <kevinrisqi22@gmail.com>
2018-12-24 00:01:56 +0700

    Membuat Interface ProdukDao dengan mengisi list yang nantinya akan di pilih

--- src/com/dotect/tokokomputer/service/ProdukDao.java
+++ src/com/dotect/tokokomputer/service/ProdukDao.java
@@ -5,10 +5,21 @@
  */
 package com.dotect.tokokomputer.service;
 
+import com.dotect.tokokomputer.entity.Produk;
+import com.dotect.tokokomputer.error.ProdukException;
+import java.util.List;
+
 /**
  *
  * @author Anonymous
  */
 public interface ProdukDao {
+    
+    public void insertProduk(Produk produk) throws ProdukException;
+    public void updateProduk(Produk produk) throws ProdukException;
+    public void deleteProduk(Integer id) throws ProdukException;
+    public void getProduk(Integer id) throws ProdukException;
+    public void getProduk(String nama) throws ProdukException;
+    public List<Produk> SelectAllProduk() throws ProdukException;
     
 }
